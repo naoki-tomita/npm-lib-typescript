ad3e147f4475d73ee205a960e2a4299e30484d0d
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-04-12 14:21:22 +0900

    Add build option.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -26,3 +26,30 @@ describe("function add", () => {
     });
   });
 });
+
+describe("function sub", () => {
+  const tests: Array<{
+    x: number;
+    y: number;
+    want: number;
+  }> = [{
+    x: 1,
+    y: 1,
+    want: 0,
+  }, {
+    x: 100000000001,
+    y: 100000000000,
+    want: 1,
+  }, {
+    x: -1,
+    y: 100,
+    want: -101,
+  }];
+
+  tests.forEach(test => {
+    const { x, y , want } = test;
+    it(`should success correctly. ${x} - ${y} = ${want}`, () => {
+      expect(sub(x, y)).toBe(want);
+    });
+  });
+});
