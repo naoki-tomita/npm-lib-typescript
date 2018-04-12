e0a4893e31198f229b32f6831158da2fc935567d
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-04-12 13:58:01 +0900

    Declare module export.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
-export declare function add(x: number, y: number): number;
-export declare function sub(x: number, y: number): number;
+declare module "npm-lib-typescript" {
+  export function add(x: number, y: number): number;
+  export function sub(x: number, y: number): number;
+}
