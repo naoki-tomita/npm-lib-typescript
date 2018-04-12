47bdf42b1dcc3b7a13e6e3208fdabbee34ef1dd3
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-04-12 13:49:12 +0900

    First commit.

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,28 @@
+import { add, sub } from "../";
+
+describe("function add", () => {
+  const tests: Array<{
+    x: number;
+    y: number;
+    want: number;
+  }> = [{
+    x: 1,
+    y: 1,
+    want: 2,
+  }, {
+    x: 100000000000,
+    y: 100000000000,
+    want: 200000000000,
+  }, {
+    x: -1,
+    y: 1,
+    want: 0,
+  }];
+
+  tests.forEach(test => {
+    const { x, y , want } = test;
+    it(`should success correctly. ${x} + ${y} = ${want}`, () => {
+      expect(add(x, y)).toBe(want);
+    });
+  });
+});
